1e5c7f5ec01e961606df802ae30804a4760253b6
ashikshezan <ashikshezan@gmail.com>
2021-06-23 12:44:12 +0600

    initial commit

---
+++ dags/a_bash_dag.py
@@ -0,0 +1,36 @@
+try:
+
+    from datetime import timedelta
+    from airflow import DAG
+    from airflow.operators.python_operator import PythonOperator
+    from airflow.operators.bash_operator import BashOperator
+    from datetime import datetime
+    import pandas as pd
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    logging.debug("All Dag modules are ok ......")
+except Exception as e:
+    print("Error  {} ".format(e))
+
+
+def first_function_execute():
+    logging.info(" ====== first_function_execute ====== ")
+
+
+with DAG(
+        dag_id="a_bash_dag",
+        template_searchpath="/opt/airflow/scripts",
+        schedule_interval="@daily",
+        default_args={
+            "owner": "airflow",
+            "retries": 1,
+            "retry_delay": timedelta(minutes=5),
+            "start_date": datetime(2021, 1, 1),
+        },
+        catchup=False) as f:
+
+    t1 = BashOperator(
+        task_id='my_bash_example',
+        # "scripts" folder is under "/usr/local/airflow/dags"
+        bash_command="bash_script.sh")
---
+++ scripts/bash_script.sh
@@ -0,0 +1,1 @@
+echo "Hello frommmmmmmmmm the script ........."
